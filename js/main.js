3b8b1f25d9e28253bea335c56c1e89cda64ee91c
Ketania Govender <2112886@students.wits.ac.za>
2022-11-01 20:51:51 +0200

    dynamic footer and header

---
+++ js/main.js
@@ -0,0 +1,76 @@
+document.body.onload = makeNav;
+function makeNav(){
+
+const header = document.createElement('header');
+const navTitle = document.createElement('a');
+navTitle.href ='../index.html';
+navTitle.innerText = "swiftify";
+navTitle.className = "navTitle"
+header.appendChild(navTitle);
+
+const navBar = document.createElement('nav');
+navBar.className = "navigation";
+header.appendChild(navBar);
+
+const navList = document.createElement('ul');
+navList.className="ulNav";
+
+const navItems =[
+    {title: 'blog', link: '../pages/blog.html'},
+    {title: 'design' , link: '../pages/design.html'},
+    {title:'data' , link: '../pages/data.html'}
+];
+
+for(let item of navItems){
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    li.className = "liNav";
+    a.className ="aNav";
+    a.innerText = item.title;
+    a.href = item.link;
+    li.appendChild(a);
+    navList.appendChild(li);
+}
+navBar.appendChild(navList);
+
+const footer = document.createElement('footer');
+footer.className = 'universalFoot';
+
+
+const credits =document.createElement('p');
+credits.innerText = "ketania govender 2022";
+footer.appendChild(credits);
+const credLinks = document.createElement('ul');
+credLinks.className ="ulFoot";
+
+const footItems = [
+    {ftitle: 'references  /', flink: '../pages/refs.html'},
+    {ftitle: 'figma  /', flink: 'https://www.figma.com/file/nXSbUGQVQb5hufBDDgLNSY/Swiftify?node-id=0%3A1'},
+    {ftitle: 'github', flink: 'https://github.com/ketaniaaa/2112886_WSOA3029'}
+
+];
+
+for(let item of footItems){
+    const fli = document.createElement('li');
+    const fa = document.createElement('a');
+    fli.className = "liFoot";
+    fa.className = "aFoot";
+    fa.innerText = item.ftitle;
+    fa.href = item.flink;
+    fli.appendChild(fa);
+    credLinks.appendChild(fli);
+}
+footer.appendChild(credLinks);
+
+
+
+
+
+
+
+
+const hedd = document.getElementById("main");
+const end = document.getElementById("end");
+document.body.insertBefore(header, hedd);
+document.body.insertBefore(footer, end )
+}
