d2c77946f1da81a9a039f4f79caa450cc7edf082
Ketania Govender <2112886@students.wits.ac.za>
2022-11-06 17:09:40 +0200

    check console

--- js/spotify.js
+++ js/spotify.js
@@ -103,7 +103,7 @@
                headers: {
                 'Authorization': 'Bearer ' + access_token
               }, success: function (data){
-             console.log("artist: " + data.tracks); 
+             console.log("title: " + data.tracks.name + " popularity: " + data.tracks.popularity); 
 
              data.tracks.map(function(title){
                 let item = $('<li>' + title.name+ '</li>');
