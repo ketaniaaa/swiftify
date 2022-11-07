a1084223a2937c4aacd2bf84316ecd217d55d006
Ketania Govender <2112886@students.wits.ac.za>
2022-11-07 07:25:22 +0200

    yup

--- js/spotify.js
+++ js/spotify.js
@@ -114,12 +114,83 @@ d3.select("#bubbleChart").data(postJSON).call(chart);
               }
 
             });
+
+            function bubbleChart(){
+
+              //parameters
+
+              var width = 500;
+              var height = 500;
+              var colRad = "popularity"; //get popularity 
+              var colCol = "name"; //artists name 
+
+              function chart(selection){
+
+                var data =
+                selection.enter().data();
+
+                var svg = d3.select("#enterSvg");
+                svg.attr('width', width).attr('height', height);
+
+                var tooltip = selection.append("#bubbleChart").style("position", "absolute").style("visibility", "hidden").style("text-decoration", "none").style("padding", "12px").style("background-color", "rgb(230, 230, 230)").style("border-radius", "4px").style("text-align", "left").style("font-family", "helvetica").style("width", "200px").style("line-height", "150%").text("");
+
+                var simulation = d3.forceSimulation(data).force("charge", d3.forceManyBody().strength([-90])).force("x", d3.forceX()).force("y", d3.forceY()).on("tick", ticked); 
+                function ticked(e){
+                  node.attr("cx", function(d) {
+                    return d.x * 1;
+                }).attr("cy", function(d) {
+                    return d.y * 1;
+                });
+                }
+                
+        var scaleRadius = d3.scaleLinear().domain([
+          d3.min(data, function(d) {
+              return + d[colRad];
+          }),
+          d3.max(data, function(d) {
+              return + d[colRad];
+          })
+      ]).range([10, 30]);
+
+      var node = svg.selectAll("circle").data(data).enter().append("circle").attr('r', function(d) {
+          return scaleRadius(d[colRad]);
+      }).style("fill", function() {
+          return '#d53e4f';
+      }).attr('transform', 'translate(' + [
+          width / 2,
+          height / 2
+      ] + ')').on("mouseover", function(d) {
+          tooltip.html(d[colCol] + "<br>" + "Followers: " + d.followers.total + "<br>" + "Popularity: " + d[colCol]);
+          return tooltip.style("visibility", "visible");
+      }).on("mousemove", function() {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+      }).on("mouseout", function() {
+          return tooltip.style("visibility", "hidden");
+      });
+  }
+
+  chart.width = function(value) {
+    if (!arguments.length) {
+        return width;
+    }
+    width = value;
+    return chart;
+};
+
+chart.height = function(value) {
+    if (!arguments.length) {
+        return height;
+    }
+    height = value;
+    return chart;
+};
+return chart;
+
+              
+            }
             
         }
-        function bubbleChart(){
-            const svg = d3.select('#bubbleChart');
-        }
-       
+         
   
   
   
@@ -127,19 +198,23 @@ d3.select("#bubbleChart").data(postJSON).call(chart);
 
       }
       
+  
     })();
     function startViz(){
         $('#login').hide();
 
-        
+        $("#enter").on('click', function(){
+          $('#enterArt').fadeIn('fast');
+          $('#enter').hide();
+          $('#folkloreBut').show();
+        });
+
+
         $("#folkloreBut").on('click', function(){
             $("#folkloreArt").fadeIn('fast');
             $("#folkloreBut").hide();
             $("#evermoreBut").show();
-
-
-
-            console.log ('fetching data');
+            $('#enterArt').hide();
          
           });
 
