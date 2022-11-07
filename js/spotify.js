0c31ac8613b9fc9ac3e9ffd652d7c9ded3b5f5c4
Ketania Govender <2112886@students.wits.ac.za>
2022-11-07 12:14:21 +0200

    idk

--- js/spotify.js
+++ js/spotify.js
@@ -163,10 +163,9 @@ d3.select("#bubbleChart").data(postJSON).call(chart);
       .on('mouseover', function(d){
        /* tooltip.html(d[colCol] + "<br>" + "popularity: " +d[colRad]);*/
         return tooltip.style("opacity", 0.9)
-        .html(d[colCol]); 
-       /* (function(d){
+        .html (function(d){
           return d.colCol + "<br>" + "popularity:" + d.colRad
-        });*/ /*
+        }); /*
 
         .style("top", (d3.event.pageY -10)+ "px")
         .style("left", (d3.event.pageX +10) + "px");*/
