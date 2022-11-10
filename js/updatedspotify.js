c09a3f5f81f60e8325003eec061f3cca172a43db
Ketania Govender <2112886@students.wits.ac.za>
2022-11-10 15:43:40 +0200

    opps

--- js/updatedspotify.js
+++ js/updatedspotify.js
@@ -199,7 +199,7 @@ $('#taylorTopBut').on('click', function(){
 pop.attr('id', 'popularityStat');
  })
 //map id data to array to get audio feature function
-                var ids = data3.tracks.map(function (singleId){
+          /*      var ids = data3.tracks.map(function (singleId){
                   return singleId.id;
                 });
                 var stringId = ids.join();
@@ -217,6 +217,7 @@ pop.attr('id', 'popularityStat');
      
     }
 }); 
+/*
 function audioFeatTop(stringId){
   $.ajax({
     url: "https://api.spotify.com/v1/audio-features?", //endpoint for getting features for multiple tracks 
@@ -268,7 +269,7 @@ var dataSet = [
   
 // must make chart 
 
-avgfeatTopTrack(dataSet); //call chart function
+avgfeatTopTrack(dataSet); //call chart function 
 
 
 
@@ -277,10 +278,10 @@ avgfeatTopTrack(dataSet); //call chart function
 
 
 
-}   
+} 
 });
-}
 
+*/
 });
 /*////////////////////////////////////////////////////////////////////////////  FOLKLORE GRAPH START//////////////////////////////////////////////////////////////////// */
 
@@ -506,144 +507,6 @@ return chart2;
 
 /*///////////////////////////////////////////////////AVERAGE AUDIO FEATURES FOR TOP TRACKS///////////////////////////////////////////////////////////////////////////////////// */
 function avgfeatTopTrack(d){
-let activeSeg;
-const data = d.sort((a,b) => b["value"] - a["value"]),
-
-vWidth = 600,
-vHeight =400,
-sWidth = vWidth,
-sHeight = vHeight,
-thickness = 35, 
-colourArray = data.map(k => k.color),
-el = d3.select("#topTrackChart"),
-radius = Math.min(sWidth, sHeight)/2, //simple mafs 
-color = d3.scaleOrdinal().range(colourArray);
-
-const max = d3.max(data, (maxD) =>
-maxD.value);
-
-
-const svg = el.append('svg')
-.attr('viewBox', `0 0 ${vWidth + thickness} ${vHeight + thickness}`)
-.attr('id', 'toptrackaud')
-.attr('width', vWidth)
-.attr('height', sHeight);
-
-
-const g = svg.append('g')
-.attr('transform', `translate( ${ (sWidth / 2) + (thickness / 2) }, ${ (sHeight / 2) + (thickness / 2)})`);
-
-const arc = d3.arc()
-  .innerRadius(radius - thickness)
-  .outerRadius(radius);
-
-  const arcHover = d3.arc()
-  .innerRadius(radius - ( thickness + 5 ))
-  .outerRadius(radius + 8);
-
-  const pie = d3.pie()
-  .value(function(dataSet) { return dataSet.value; })
-  .sort(null);
-
-
-  const path = g.selectAll('path')
-  .attr('class', 'data-path')
-  .data(pie(data))
-  .enter()
-  .append('g')
-  .attr('class', 'data-group')
-  .each(function(pathData, i) {
-    const group = d3.select(this)
-
-    group.append('text')
-      .text(`${pathData.data.value}`)
-      .attr('class', 'data-text data-text__value')
-      .attr('text-anchor', 'middle')
-      .attr('dy', '1rem')
-
-    group.append('text')
-      .text(`${pathData.data.name}`)
-      .attr('class', 'data-text data-text__name')
-      .attr('text-anchor', 'middle')
-      .attr('dy', '3.5rem')
-
-    // Set default active segment
-    if (pathData.value === max) {
-      const textVal = d3.select(this).select('.data-text__value')
-      .classed('data-text--show', true);
-
-      const textName = d3.select(this).select('.data-text__name')
-      .classed('data-text--show', true);
-    }
-
-  })
-  .append('path')
-  .attr('d', arc)
-  .attr('fill', (fillData, i) => color(fillData.data.name))
-  .attr('class', 'data-path')
-  .on('mouseover', function() {
-    const _thisPath = this,
-          parentNode = _thisPath.parentNode;
-
-    if (_thisPath !== activeSegment) {
-
-      activeSegment = _thisPath;
-
-      const dataTexts = d3.selectAll('.data-text')
-      .classed('data-text--show', false);
-
-      const paths = d3.selectAll('.data-path')
-      .transition()
-      .duration(250)
-      .attr('d', arc);
-
-      d3.select(_thisPath)
-        .transition()
-        .duration(250)
-        .attr('d', arcHover);
-
-      const thisDataValue = d3.select(parentNode).select('.data-text__value')
-      .classed('data-text--show', true);
-      const thisDataText = d3.select(parentNode).select('.data-text__name')
-      .classed('data-text--show', true);
-    }
-
-
-  })
-  .each(function(v, i) {
-    if (v.value === max) {
-      const maxArc = d3.select(this)
-      .attr('d', arcHover);
-      activeSegment = this;
-    }
-    this._current = i;
-  });
-
-  const legendRectSize = 15;
-  const legendSpacing = 10;
-
-  const legend = svg.selectAll('.legend')
-  .data(color.domain())
-  .enter()
-  .append('g')
-  .attr('class', 'legend')
-  .attr('transform', function(legendData, i) {
-    const itemHeight =    legendRectSize + legendSpacing;
-    const offset =        legendRectSize * color.domain().length;
-    const horz =          svgWidth + 80;
-    const vert =          (i * itemHeight) + legendRectSize + (svgHeight - offset) / 2;
-    return `translate(${horz}, ${vert})`;
-  });
-
-  legend.append('circle')
-    .attr('r', legendRectSize / 2)
-    .style('fill', color);
-
-  legend.append('text')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .attr('class', 'legend-text')
-    .text( (legendData) => legendData )
 
 
 }
