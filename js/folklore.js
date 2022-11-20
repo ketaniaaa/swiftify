3bd770f8cf34d1d6b75dcbef2ea5ea3b3364687e
Ketania Govender <82605074+ketaniaaa@users.noreply.github.com>
2022-11-21 00:14:25 +0200

    new charts

--- js/folklore.js
+++ js/folklore.js
@@ -1,4 +1,738 @@
-const data=[
+function chart1(){
+ 
+  $("#chart1").fadeIn('fast');
+  $("#chart3").hide();
+  $("#chart2").hide();
+  $("#chart4").hide();
+
+  var margin = {top: 10, right: 40, bottom: 30, left: 30},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#chart1")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+      console.log("d3 has begun")
+
+      var data = [
+        {
+            name: "energy",
+            color: "#DAE2E7C7",
+            value: 36
+        },
+        {
+            name: "dance",
+            color: "#2C3022C7",
+            value: 77
+        },
+        {
+            name: "speechiness",
+            color: "#919F65C7",
+            value: 52
+        },
+        {
+            name: "acousticness",
+            color: "#56764DC7",
+            value: 76
+        },
+       /* */
+        {
+            name: "valence",
+            color: "#4F5E46C7",
+            value: 18
+        },
+        {
+            name: "liveliness",
+            color: "#000000",
+            value: 12
+        },
+      ];
+
+       xAxis = d3.scaleLinear()
+      .domain([0,170])
+      .range([0, width]);
+
+     var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.name; }));
+/*
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+       // .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");*/
+      /*svg.append('g')
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xAxis));*/
+
+      var y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([ height, 0]);
+
+      const rVal = d3.scaleLinear().domain([
+        d3.min(data, function(d){
+            return + d.value
+        }),
+        d3.max(data, function(d){
+            return + d.value
+        })
+      ]).range([0, 100]);
+
+     var cx = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right +200])
+      .padding(1);
+     
+      var node = svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cy", 200)
+      .attr("cx", (d, i)=> cx(i))
+      .attr("r", (d) => rVal(d.value))
+      .attr("fill", function(d){
+        return d.color;
+      });
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 135)
+      .attr("x2", 135)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 240)
+      .attr("x2", 240)
+      .attr("y1", 200)
+      .attr("y2", 360); 
+      
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 350)
+      .attr("x2", 350)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 460)
+      .attr("x2", 460)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 572)
+      .attr("x2", 572)
+      .attr("y1", 200)
+      .attr("y2", 360);
+ ///////////////////////////////////////////remember to change this     
+svg.append("text") //energy
+.text("0.36")
+.attr("x", 120)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+     
+svg.append("text") //dance
+.text("0.77")
+.attr("x", 220)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //speechiness
+.text("0.52")
+.attr("x", 330)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //acous
+.text("0.76")
+.attr("x", 440)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //valence
+.text("0.18")
+.attr("x", 550)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+}
+
+function chart2(){
+  
+  $("#chart2").fadeIn('fast');
+  $("#chart1").hide();
+  $("#chart3").hide();
+  $("#chart4").hide();
+
+
+  var margin = {top: 10, right: 40, bottom: 30, left: 30},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#chart2")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+      console.log("d3 has begun")
+
+      var data = [
+        {
+            name: "energy",
+            color: "#DAE2E7C7",
+            value: 59
+        },
+        {
+            name: "dance",
+            color: "#2C3022C7",
+            value: 61
+        },
+        {
+            name: "speechiness",
+            color: "#919F65C7",
+            value: 10
+        },
+        {
+            name: "acousticness",
+            color: "#56764DC7",
+            value: 53
+        },
+       /* */
+        {
+            name: "valence",
+            color: "#4F5E46C7",
+            value: 55
+        },
+        
+      ];
+
+       xAxis = d3.scaleLinear()
+      .domain([0,170])
+      .range([0, width]);
+
+     var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.name; }));
+     var y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([ height, 0]);
+
+      const rVal = d3.scaleLinear().domain([
+        d3.min(data, function(d){
+            return + d.value
+        }),
+        d3.max(data, function(d){
+            return + d.value
+        })
+      ]).range([0, 100]);
+
+     var cx = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right +200])
+      .padding(1);
+     
+      var node = svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cy", 200)
+      .attr("cx", (d, i)=> cx(i))
+      .attr("r", (d) => rVal(d.value))
+      .attr("fill", function(d){
+        return d.color;
+      });
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 145)
+      .attr("x2", 145)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 260)
+      .attr("x2", 260)
+      .attr("y1", 200)
+      .attr("y2", 360); 
+      
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 350)
+      .attr("x2", 350)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 490)
+      .attr("x2", 490)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 650)
+      .attr("x2", 650)
+      .attr("y1", 200)
+      .attr("y2", 360);
+ ///////////////////////////////////////////remember to change this     
+svg.append("text") //energy
+.text("0.59")
+.attr("x", 120)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+     
+svg.append("text") //dance
+.text("0.61")
+.attr("x", 220)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //speechiness
+.text("0.04")
+.attr("x", 330)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //acous
+.text("0.53")
+.attr("x", 440)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //valence
+.text("0.55")
+.attr("x", 600)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+}
+
+
+
+
+
+
+function chart3(){
+  $("#chart3").fadeIn('fast');
+  $("#chart1").hide();
+  $("#chart2").hide();
+  $("#chart4").hide();
+
+  var margin = {top: 10, right: 40, bottom: 30, left: 30},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#chart3")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+      console.log("d3 has begun")
+
+      var data = [
+        {
+            name: "energy",
+            color: "#DAE2E7C7",
+            value: 59
+        },
+        {
+            name: "dance",
+            color: "#2C3022C7",
+            value: 69
+        },
+        {
+            name: "speechiness",
+            color: "#919F65C7",
+            value: 41
+        },
+        {
+            name: "acousticness",
+            color: "#56764DC7",
+            value: 47
+        },
+       /* */
+        {
+            name: "valence",
+            color: "#4F5E46C7",
+            value: 71
+        },
+       
+      ];
+
+       xAxis = d3.scaleLinear()
+      .domain([0,170])
+      .range([0, width]);
+
+     var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.name; }));
+
+
+      var y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([ height, 0]);
+
+      const rVal = d3.scaleLinear().domain([
+        d3.min(data, function(d){
+            return + d.value
+        }),
+        d3.max(data, function(d){
+            return + d.value
+        })
+      ]).range([0, 100]);
+
+     var cx = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right +200])
+      .padding(1);
+     
+      var node = svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cy", 200)
+      .attr("cx", (d, i)=> cx(i))
+      .attr("r", (d) => rVal(d.value))
+      .attr("fill", function(d){
+        return d.color;
+      });
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 135)
+      .attr("x2", 135)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 240)
+      .attr("x2", 240)
+      .attr("y1", 200)
+      .attr("y2", 360); 
+      
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 350)
+      .attr("x2", 350)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 460)
+      .attr("x2", 460)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 572)
+      .attr("x2", 572)
+      .attr("y1", 200)
+      .attr("y2", 360);
+ ///////////////////////////////////////////remember to change this     
+svg.append("text") //energy
+.text("0.59")
+.attr("x", 120)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+     
+svg.append("text") //dance
+.text("0.69")
+.attr("x", 220)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //speechiness
+.text("0.41")
+.attr("x", 330)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //acous
+.text("0.47")
+.attr("x", 440)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //valence
+.text("0.71")
+.attr("x", 550)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+}
+
+function chart4(){
+  $("#chart4").fadeIn('fast');
+  $("#chart1").hide();
+  $("#chart2").hide();
+  $("#chart3").hide();
+
+  var margin = {top: 10, right: 40, bottom: 30, left: 30},
+    width = 700 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+var svg = d3.select("#chart4")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+      console.log("d3 has begun")
+
+      var data = [
+        {
+            name: "energy",
+            color: "#DAE2E7C7",
+            value: 38
+        },
+        {
+            name: "dance",
+            color: "#2C3022C7",
+            value: 30
+        },
+        {
+            name: "speechiness",
+            color: "#919F65C7",
+            value: 3
+        },
+        {
+            name: "acousticness",
+            color: "#56764DC7",
+            value: 78
+        },
+       /* */
+        {
+            name: "valence",
+            color: "#4F5E46C7",
+            value: 15
+        },
+       
+      ];
+
+       xAxis = d3.scaleLinear()
+      .domain([0,170])
+      .range([0, width]);
+
+     var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.name; }));
+
+
+      var y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([ height, 0]);
+
+      const rVal = d3.scaleLinear().domain([
+        d3.min(data, function(d){
+            return + d.value
+        }),
+        d3.max(data, function(d){
+            return + d.value
+        })
+      ]).range([0, 100]);
+
+     var cx = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right +200])
+      .padding(1);
+     
+      var node = svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cy", 200)
+      .attr("cx", (d, i)=> cx(i))
+      .attr("r", (d) => rVal(d.value))
+      .attr("fill", function(d){
+        return d.color;
+      });
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 155)
+      .attr("x2", 155)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 260)
+      .attr("x2", 260)
+      .attr("y1", 200)
+      .attr("y2", 360); 
+      
+      
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 370)
+      .attr("x2", 370)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 480)
+      .attr("x2", 480)
+      .attr("y1", 200)
+      .attr("y2", 360);
+
+      svg.append("g")
+      .attr("stroke", "#000000")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", 3,3)
+    .selectAll("line")
+    .data(data)
+    .join("line")
+      .attr("x1", 660)
+      .attr("x2", 660)
+      .attr("y1", 200)
+      .attr("y2", 360);
+ ///////////////////////////////////////////remember to change this     
+svg.append("text") //energy
+.text("0.38")
+.attr("x", 120)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+     
+svg.append("text") //dance
+.text("0.30")
+.attr("x", 220)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //speechiness
+.text("0.03")
+.attr("x", 330)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //acous
+.text("0.78")
+.attr("x", 440)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+
+svg.append("text") //valence
+.text(".15")
+.attr("x", 630)
+.attr("y", 380)
+.attr("id", "songFeatLabel");
+}
+/*const data=[
     { title: 'the 1', popularity: 64},
     { title: 'cardigan', popularity: 65},
     { title: 'the last great american dynasty', popularity: 63},
@@ -18,8 +752,8 @@ const data=[
     { title: 'the lakes - bonus track', popularity: 65}
 ];
 
-const width = 1000;
-const height = 800;
+const width = 600;
+const height = 400;
 const margin = {top: 50, bottom: 50, left:50, right:50};
 
 const svg = d3.select('#folkloreContainer')
@@ -72,4 +806,4 @@ g.attr('transform', 'translate(${margin.left},0)')
 };
 svg.append('g').call(yAxis);
 svg.append('g').call(xAxis);
-svg.node();
+svg.node();*/
